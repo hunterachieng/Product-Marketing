26fe269023b86547455cf630fcbc387abf253df6
hunterachieng <achienghunter@gmail.com>
2021-08-11 18:47:19 +0300

    no modal

--- src/Home.js
+++ src/Home.js
@@ -6,19 +6,17 @@ import sony from './Images/watch2.jpg';
 import grey from './Images/watch4.jpg';
 import Layout from './Layout';
 import Footer from './Footer';
-import Modal from './Modal';
+
 
 
 
 
 export default function Home(){
-    const [show, setShow] = useState(true);
-    
+ 
 
     return(
         <div className="home">
             <Layout/>
-            <Modal show = {show} setShow = {()=> setShow(false)}/>
             <p className="pitch"> As the year comes to an end, Superb is pleased to announce the<br/>
              introduction of new products from our summer collection. Just<br/>
              as clothes are important, technology is also needed in our daily<br/>
